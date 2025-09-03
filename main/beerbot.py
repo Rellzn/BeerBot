4816fa4712c4010dd9f2652489a54bc42b0b0639
Matthew Miller <mmi1798@wgu.edu>
2025-09-03 03:37:32 -0400

    Removed unused imports

--- main/beerbot.py
+++ main/beerbot.py
@@ -1,12 +1,7 @@
 ## Installing necessary libraries
 
-import numpy as np
 import pandas as pd
 import sklearn
-import matplotlib.pyplot as plt
-import seaborn as sns
-import warnings 
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
 ## Loading the dataset
 beers = pd.read_csv('data/beer_reviews.csv')
